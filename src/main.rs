d952ae40e394af4c456310990a8ac9891dd87eb2
eyemotif <yepicness@gmail.com>
2023-03-07 16:15:44 -0500

    Layout basic files+execution

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+mod cli;
+mod http;
+
+fn run() -> Result<(), Box<dyn std::error::Error>> {
+    Ok(())
+}
+
+fn main() -> std::process::ExitCode {
+    match run() {
+        Ok(()) => std::process::ExitCode::SUCCESS,
+        Err(err) => {
+            eprintln!("{err}");
+            std::process::ExitCode::FAILURE
+        }
+    }
 }
